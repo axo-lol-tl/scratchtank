6ed7b494fd78733af32fd57d64be2d619191fd70
mggower <gowermikey@gmail.com>
2020-11-20 14:37:14 -0500

    Merge OAUTH to PR

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google: {
+    GOOGLE_CLIENT_ID:
+      '1019100131601-i3kb76du4snnbegvqcddnjug33ooje2e.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: 'EEQtl-m2j3lyO2bAdkjjXwww',
+  },
+};
