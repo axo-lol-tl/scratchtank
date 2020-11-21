6514c945f779e6b5b3b9719b700b7a110f1fadd4
Edward Cho <edwardcho1231@gmail.com>
2020-11-21 09:56:02 -0500

    extracted db uri to keys.js

--- server/keys.js
+++ server/keys.js
@@ -4,7 +4,9 @@ module.exports = {
       '1019100131601-i3kb76du4snnbegvqcddnjug33ooje2e.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: 'EEQtl-m2j3lyO2bAdkjjXwww',
   },
-
+  postgreSQL: {
+    URI: 'postgres://swwzsbqk:2H0DivQiFgEomiUWwrREzWSeNVcfGAnN@rajje.db.elephantsql.com:5432/swwzsbqk',
+  },
   session: {
     cookieKey: 'axolotlcuties',
   },
